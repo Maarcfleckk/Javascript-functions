4923d2f2cc84c7f75e237f4969dc380f7460119c
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-11-18 18:20:18 +0100

    Finish exercise 2

--- exercise_02/getTotalWordsFromString.js
+++ exercise_02/getTotalWordsFromString.js
@@ -3,3 +3,20 @@
  * input: string (sentence)
  * output: number
  */
+
+/**
+ * Calculates the total number of words in a given string.
+ * @param {string} sentence - The input string (sentence).
+ * @returns {number} - The total number of words in the sentence.
+ */
+function getTotalWordsFromString(sentence) {
+  if (typeof sentence !== "string") {
+    console.log("It must be a String")
+  }
+
+  let words = sentence.split(/\s+/);
+  let filteredWords = words.filter((word) => word.length > 0);
+  return filteredWords.length;
+}
+
+export default getTotalWordsFromString;
