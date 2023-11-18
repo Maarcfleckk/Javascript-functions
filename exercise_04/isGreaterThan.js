0ff3ee96cddb6e6a2ad82bbee79d2f61f7338cfe
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-11-18 18:29:37 +0100

    Finish exercise 4

--- exercise_04/isGreaterThan.js
+++ exercise_04/isGreaterThan.js
@@ -4,3 +4,18 @@
  * input: number (threshold)
  * output: boolean
  */
+
+const isGreaterThan = function isGreaterThan(value, threshold){
+    if (typeof value !== "number" || typeof threshold !== "number") {
+        console.log("Input must be a number");
+    }
+    if(value > threshold){
+        return value + " is greater than " + threshold;
+    }
+    if(value < threshold){
+        return value + " is smaller than " + threshold;
+    }
+    return value+" is equal to "+threshold;
+}
+
+export default isGreaterThan;
