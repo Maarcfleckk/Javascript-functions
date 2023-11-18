758bf3377acf6eaa5d3fd2b517c276a2d1b9b01b
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-11-18 18:23:57 +0100

    Finish exercise 3

--- exercise_03/generateRandomNumberInRange.js
+++ exercise_03/generateRandomNumberInRange.js
@@ -3,3 +3,20 @@
  * input: number (limit)
  * output: number
  */
+
+/**
+ * Generates a rounded random number within the specified range.
+ * @param {number} limit - The upper limit for the random number (exclusive).
+ * @returns {number} - The generated random number.
+ */
+function generateRandomNumberInRange(limit) {
+  if (typeof limit !== "number") {
+    console.log("Input must be a number");
+  }
+
+  let randomNumber = Math.random() * limit;
+  let roundedRandomNumber = Math.round(randomNumber);
+  return roundedRandomNumber;
+}
+
+export default generateRandomNumberInRange;
