9392ca8e62d82e7ea776094a7606ade56b43cb4b
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-11-21 22:59:27 +0100

    Finish exercise 10 and fix exercise 9 index

--- exercise_10/transformCollectionToString.js
+++ exercise_10/transformCollectionToString.js
@@ -1,3 +1,4 @@
+import checkCollectionHasElements from "../exercise_06/checkCollectionHasElements.js";
 /**
  * Write JS function "transformCollectionToString" to transform collection into string
     - if array has elements, then return array as string, joined by '|' (pipe character)
@@ -17,3 +18,15 @@
  * input: []
  * output: ""
  */
+
+/**
+ * Transforms a collection into a string.
+ * @param {array} collection 
+ * @returns {string}
+ */
+const transformCollectionToString = function(collection){
+   const hasElements = checkCollectionHasElements(collection);
+
+   return hasElements ? collection.join(' '): '';
+}
+export default transformCollectionToString;
