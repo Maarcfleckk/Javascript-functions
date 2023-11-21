37150d565ba07db52d499034588305f150622a7a
Fleki <marc.fleck@mataro.epiaedu.cat>
2023-11-21 20:52:18 +0100

    Finish exercise 8

--- exercise_09/getDistanceMessageFromSumTo100.js
+++ exercise_09/getDistanceMessageFromSumTo100.js
@@ -1,3 +1,5 @@
+import generateRandomNumberInRange from "../exercise_03/generateRandomNumberInRange.js";
+import isAdditionGreaterThanFifty from "../exercise_05/isAdditionGreaterThanFifty.js";
 /** 
  * Write JS function "getDistanceMessageFromSumTo100" to generate a random number (in between 0-100) and sum it to given number
   - if sum result exceeds 100, then return message "Sum with value N exceeds in N from number 100"
@@ -17,5 +19,20 @@
  * CASE #2
  * input: 30
  * random (generated in function): 40
- * output: "Sum with value 70 isd left in 30 from number 100"
+ * output: "Sum with value 70 is left in 30 from number 100"
  */
+
+/**
+ * 
+ * @param {number} value 
+ * @returns {string}
+ */
+const getDistanceMessageFromSumTo100 = function(value){
+    const number = generateRandomNumberInRange(101); //if the param is 101 the limit will be 100
+    const foo = isAdditionGreaterThanFifty(number, value);
+    
+    return foo
+      ? "Sum with value " + (number + value) + " exceeds in " + (value + number - 100) + " from number 100"
+      : "Sum with value " + (number + value) + " is left in " + (100 - value - number) + " from number 100";
+}
+export default getDistanceMessageFromSumTo100;
--- exercise_09/index.js
+++ exercise_09/index.js
@@ -1,1 +1,7 @@
-//imports
+import getDistanceMessageFromSumTo100 from "./getDistanceMessageFromSumTo100.js";
+
+const result1 = 100;
+console.log(getDistanceMessageFromSumTo100(100));
+
+const result2 = 0;
+console.log(getDistanceMessageFromSumTo100(result2));
